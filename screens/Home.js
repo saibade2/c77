d83c776e340fa303e76f991e81ed6a30e9c38f18
Sai Bade <90086494+saibade2@users.noreply.github.com>
2023-05-29 10:01:56 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -26,10 +26,10 @@ export default class HomeScreen extends Component {
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
                 */}
-                {/* <ImageBackground 
+                 <ImageBackground 
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
-                */}
+                
                 {/* <BackgroundImage
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
@@ -52,10 +52,10 @@ export default class HomeScreen extends Component {
                           style={styles.routeCard} onPress={() =>
                         this.props("SpaceCraft")
                     }> */}
-                    {/* <TouchableOpacity 
+                     <TouchableOpacity 
                           style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("SpaceCraft")
-                    }> */}                    
+                    }>                   
                         <Text style={styles.routeText}>Spacecrafts</Text>
                         <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
